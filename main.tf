f5b4a83e6843a62ffb84570874afb88e6c40269f
ArtyomKr <tyomik.krasnitsky@gmail.com>
2025-06-07 22:14:28 +0300

    init: created a main terraform file

---
+++ main.tf
@@ -0,0 +1,24 @@
+variable "region" { default = "eu-central-1" }
+
+terraform {
+  backend "s3" {
+    bucket = "artyomkr-terraform-state"
+    key    = "rsschool/state"
+    region = "eu-central-1"
+    encrypt = "true"
+    use_lockfile = "true"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.region
+}
