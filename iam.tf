110584e684e0e7f49e4e2bbfeabddc6cef11986e
ArtyomKr <tyomik.krasnitsky@gmail.com>
2025-06-11 21:31:26 +0300

    refactor: remove branch push restriction

--- iam.tf
+++ iam.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "github_actions_role" {
       Condition = {
         StringEquals = {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:ref:refs/heads/main"
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:*"
         }
       }
     }]
