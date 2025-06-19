98b8df61b1f9e3cc9bacf1ad8030832da8408c75
ArtyomKr <tyomik.krasnitsky@gmail.com>
2025-06-19 18:53:26 +0300

    feat: add route table associations

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,23 +1,73 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_1a" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
   availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "public_1b" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-central-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_1a" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
   availability_zone = "eu-central-1a"
 }
 
-resource "aws_internet_gateway" "internet_gateway" {
+resource "aws_subnet" "private_1b" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "eu-central-1a"
+}
+
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_route_table" "route_table" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block  = "0.0.0.0/0"
+    instance_id = aws_instance.nat.id
+  }
+}
+
+resource "aws_route_table_association" "public_1a" {
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1b" {
+  subnet_id      = aws_subnet.public_1b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_1a" {
+  subnet_id      = aws_subnet.private_1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_1b" {
+  subnet_id      = aws_subnet.private_1b.id
+  route_table_id = aws_route_table.private.id
 }
