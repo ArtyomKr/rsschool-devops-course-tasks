b472ff5645113c94bcdc122c2c705ec7917fd617
ArtyomKr <tyomik.krasnitsky@gmail.com>
2025-06-21 13:52:39 +0300

    refactor: modify private sg

--- terraform/modules/networking/security_groups.tf
+++ terraform/modules/networking/security_groups.tf
@@ -51,10 +51,10 @@ resource "aws_security_group" "private" {
   }
 
   ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion.id]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.main.cidr_block]
   }
 }
 
