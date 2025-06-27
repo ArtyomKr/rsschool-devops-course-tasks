64522b1d2f887a723ab42e632ea2242c302dbd90
ArtyomKr <tyomik.krasnitsky@gmail.com>
2025-06-27 09:57:12 +0300

    feat: fix self-reference for k3s server

--- terraform/modules/k3s/k3_server.tf
+++ terraform/modules/k3s/k3_server.tf
@@ -10,8 +10,6 @@ resource "aws_instance" "k3s_server" {
     curl -sfL https://get.k3s.io | sh -s - server \
       --cluster-init \
       --token=${random_password.k3s_token.result} \
-      --bind-address=${aws_instance.k3s_server.private_ip} \
-      --advertise-address=${aws_instance.k3s_server.private_ip}
   EOF
 
   tags = {
