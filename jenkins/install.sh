e6c36b031f403742862607430b3b58c2c88c13d9
ArtyomKr <tyomik.krasnitsky@gmail.com>
2025-07-02 21:34:22 +0300

    feat: add scripts for jenkins installation and running

---
+++ jenkins/install.sh
@@ -0,0 +1,11 @@
+# Create PV and StorageClass
+kubectl apply -f jenkins-01-volume.yaml
+
+# Set permissions on the volume (inside Minikube)
+minikube ssh -- "sudo mkdir -p /data/jenkins-volume && sudo chown -R 1000:1000 /data/jenkins-volume"
+
+# Create ServiceAccount
+kubectl apply -f jenkins-02-sa.yaml
+
+# Install Jenkins via Helm
+helm install jenkins -n devops-tools -f jenkins-values.yaml jenkinsci/jenkins
